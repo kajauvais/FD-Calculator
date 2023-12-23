92a4bcb408681c84969b63b7d4284cd5ac09adfc
Kaja <139108622+kajauvais@users.noreply.github.com>
2023-12-23 18:54:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function fdRate() {
+    const month = Number(document.getElementById("input").value);
+    let rate = 0
+    
+
+    if (month<3) {
+        rate = 5.8
+    }
+    else if(month>=3 && month<=6){
+        rate = 6.5
+    }
+    else if(month>=7 && month<=9){
+        rate = 6.8
+    }
+    else{
+        rate = 7
+    }
+
+    let result = document.getElementById('result');
+    result.innerHTML = 'Your interest rate is '+ rate + '%';
+}
+
+
+
+
+
